fe904a8509a830a2f031e92617144ac69d60f67c
rtinsley <roger.tinsley@asidua.com>
2015-02-05 14:17:39 +0000

    Initial commit

---
+++ TableStorage/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table.DataServices;
+
+namespace TableStorage
+{
+    public partial class Country : TableServiceEntity
+    {
+        public Country(string name, string continent)
+        {
+            PartitionKey = continent;
+            RowKey = name;
+        }
+
+        public Country()
+        {
+
+        }
+        
+        public string Language { get; set; }
+        public string Population { get; set; }
+    }
+}
---
+++ TableStorage/Models/CountryMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TableStorage
+{
+    [MetadataType(typeof(CountryMetadata))]
+    public partial class Country { }
+
+    public class CountryMetadata
+    {
+        [Display(Name="Continent")]
+        public string PartitionKey { get; set; }
+
+        [Display(Name="Name")]
+        public string RowKey { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTimeOffset Timestamp { get; set; }
+
+    }
+}
